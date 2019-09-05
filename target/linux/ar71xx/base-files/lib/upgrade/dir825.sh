QbikEdge/openwrt

commit 06200cf2fb2565d3880714ba1569fff0c0720265

treewide: replace remaining (not working now) $SAVE_CONFIG uses
This var has been replaced by the $UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG

Fixes: 3b62e04112b1 ("base-files: pass "save_config" option to the "sysupgrade" method")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
@@ -75,7 +75,7 @@ dir825b_do_upgrade_combined() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \

target/linux/ar71xx/base-files/lib/upgrade/openmesh.sh
@@ -159,7 +159,7 @@ platform_do_upgrade_openmesh()
 	local cfg_size= kernel_size= rootfs_size=
 	local append=""
 
-	[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+	[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 	cfg_size=$(dd if="$img_path" bs=2 skip=35 count=4 2>/dev/null)
 	kernel_size=$(dd if="$img_path" bs=2 skip=71 count=4 2>/dev/null)

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -65,7 +65,7 @@ platform_do_upgrade_combined() {
 	then
 		local rootfspart=$(platform_find_rootfspart "$partitions" "$kernelpart")
 		local append=""
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		if [ "$PLATFORM_DO_UPGRADE_COMBINED_SEPARATE_MTD" -ne 1 ]; then
 		    ( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
@@ -164,7 +164,7 @@ platform_do_upgrade_compex() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \

target/linux/ath25/base-files/lib/upgrade/platform.sh
@@ -67,7 +67,7 @@ platform_do_upgrade() {
 	   [ ${erase_size:-0} -gt 0 ];
 	then
 		local append=""
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
 		  dd if="$1" bs=$CI_BLKSZ skip=$((1+$kern_blocks)) count=$root_blocks 2>/dev/null ) | \

target/linux/ath79/base-files/lib/upgrade/platform.sh
@@ -14,7 +14,7 @@ redboot_fis_do_upgrade() {
 	if [ "$magic" = "4349" ]; then
 		local kern_length=0x$(dd if="$sysup_file" bs=2 skip=1 count=4 2>/dev/null)
 
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 		dd if="$sysup_file" bs=64k skip=1 2>/dev/null | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 
@@ -22,7 +22,7 @@ redboot_fis_do_upgrade() {
 		local board_dir=$(tar tf $sysup_file | grep -m 1 '^sysupgrade-.*/$')
 		local kern_length=$(tar xf $sysup_file ${board_dir}kernel -O | wc -c)
 
-		[ -f "$CONF_TAR" -a "$SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 		tar xf $sysup_file ${board_dir}kernel ${board_dir}root -O | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 

target/linux/imx6/base-files/lib/upgrade/platform.sh
@@ -75,7 +75,7 @@ platform_pre_upgrade() {
 
 	case "$board" in
 	apalis*)
-		[ "$SAVE_CONFIG" -eq 0 ] && {
+		[ "$UPGRADE_OPT_SAVE_CONFIG" -eq 0 ] && {
 			jffs2reset -y
 			umount /overlay
 		}

target/linux/ipq40xx/base-files/lib/upgrade/openmesh.sh
@@ -74,7 +74,7 @@ platform_do_upgrade_openmesh() {
 	#
 
 	# take care of restoring a saved config
-	[ "$SAVE_CONFIG" -eq 1 ] && restore_backup="${MTD_CONFIG_ARGS} -j ${CONF_TAR}"
+	[ "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && restore_backup="${MTD_CONFIG_ARGS} -j ${CONF_TAR}"
 
 	mtd -q erase inactive
 	tar xf $tar_file ${board_dir}/root -O | mtd -n -p $kernel_length $restore_backup write - $PART_NAME

target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -37,7 +37,7 @@ zyxel_do_upgrade() {
 
 	tar Oxf $tar_file ${board_dir}/kernel | mtd write - kernel
 
-	if [ "$SAVE_CONFIG" -eq 1 ]; then
+	if [ "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ]; then
 		tar Oxf $tar_file ${board_dir}/root | mtd -j "$CONF_TAR" write - rootfs
 	else
 		tar Oxf $tar_file ${board_dir}/root | mtd write - rootfs

commit d8eb6a5a15ee56b8ed75ddecfc1cc2b65e3a4eba

treewide: fix invalid UPGRADE_OPT_SAVE_CONFIG spellings
That was a result of accidentally running "sed" twice on some files.

Fixes: 06200cf2fb25 ("treewide: replace remaining (not working now) $SAVE_CONFIG uses")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
@@ -75,7 +75,7 @@ dir825b_do_upgrade_combined() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \

target/linux/ar71xx/base-files/lib/upgrade/openmesh.sh
@@ -159,7 +159,7 @@ platform_do_upgrade_openmesh()
 	local cfg_size= kernel_size= rootfs_size=
 	local append=""
 
-	[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+	[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 	cfg_size=$(dd if="$img_path" bs=2 skip=35 count=4 2>/dev/null)
 	kernel_size=$(dd if="$img_path" bs=2 skip=71 count=4 2>/dev/null)

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -65,7 +65,7 @@ platform_do_upgrade_combined() {
 	then
 		local rootfspart=$(platform_find_rootfspart "$partitions" "$kernelpart")
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		if [ "$PLATFORM_DO_UPGRADE_COMBINED_SEPARATE_MTD" -ne 1 ]; then
 		    ( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
@@ -164,7 +164,7 @@ platform_do_upgrade_compex() {
 
 	if [ -n "$fw_mtd" ] &&  [ ${fw_blocks:-0} -gt 0 ]; then
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		sync
 		dd if="$fw_file" bs=64k skip=1 count=$fw_blocks 2>/dev/null | \

target/linux/ath25/base-files/lib/upgrade/platform.sh
@@ -67,7 +67,7 @@ platform_do_upgrade() {
 	   [ ${erase_size:-0} -gt 0 ];
 	then
 		local append=""
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 
 		( dd if="$1" bs=$CI_BLKSZ skip=1 count=$kern_blocks 2>/dev/null; \
 		  dd if="$1" bs=$CI_BLKSZ skip=$((1+$kern_blocks)) count=$root_blocks 2>/dev/null ) | \

target/linux/ath79/base-files/lib/upgrade/platform.sh
@@ -14,7 +14,7 @@ redboot_fis_do_upgrade() {
 	if [ "$magic" = "4349" ]; then
 		local kern_length=0x$(dd if="$sysup_file" bs=2 skip=1 count=4 2>/dev/null)
 
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 		dd if="$sysup_file" bs=64k skip=1 2>/dev/null | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 
@@ -22,7 +22,7 @@ redboot_fis_do_upgrade() {
 		local board_dir=$(tar tf $sysup_file | grep -m 1 '^sysupgrade-.*/$')
 		local kern_length=$(tar xf $sysup_file ${board_dir}kernel -O | wc -c)
 
-		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
+		[ -f "$CONF_TAR" -a "$UPGRADE_OPT_UPGRADE_OPT_SAVE_CONFIG" -eq 1 ] && append="-j $CONF_TAR"
 		tar xf $sysup_file ${board_dir}kernel ${board_dir}root -O | \
 			mtd -r $append -F$kern_part:$kern_length:0x80060000,rootfs write - $kern_part:rootfs
 