QbikEdge/openwrt

commit 8c51c7fd48d64d8491f900839c1dbc6151f1a772

ar71xx: add support for TP-LINK CPE510 V2.0
Adds Support for the TP-LINK CPE510 V2.0 by TP-Link.
The hardware is almost the same as the CPE510 V1.0
Follow the same processes as for the CPE510 V1.0

Signed-off-by: Andrew Cameron <apcameron@softhome.net>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -585,7 +585,14 @@ ar71xx_board_detect() {
 		;;
 	*"CPE510/520")
 		name="cpe510"
-		tplink_pharos_board_detect "$(tplink_pharos_get_model_string | tr -d '\r')"
+		tplink_pharos_board_detect "$(tplink_pharos_v2_get_model_string)"
+		case $AR71XX_MODEL in
+		'TP-Link CPE510 v2.0')
+			;;
+		*)
+			tplink_pharos_board_detect "$(tplink_pharos_get_model_string | tr -d '\r')"
+			;;
+		esac
 		;;
 	*"CPE830")
 		name="cpe830"

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -582,7 +582,6 @@ platform_check_image() {
 		return $?
 		;;
 	cpe210|\
-	cpe510|\
 	eap120|\
 	wbs210|\
 	wbs510)
@@ -592,6 +591,20 @@ platform_check_image() {
 	cpe210-v2)
 		tplink_pharos_check_image "$1" "01000000" "$(tplink_pharos_v2_get_model_string)" '\0\xff\r' && return 0
 		return 1
+		;;
+	cpe510)
+		local modelstr="$(tplink_pharos_v2_get_model_string)"
+		tplink_pharos_board_detect $modelstr
+		case $AR71XX_MODEL in
+		'TP-Link CPE510 v2.0')
+			tplink_pharos_check_image "$1" "7f454c46" "$modelstr" '\0\xff\r' && return 0
+			return 1
+			;;
+		*)
+			tplink_pharos_check_image "$1" "7f454c46" "$(tplink_pharos_get_model_string)" '' && return 0
+			return 1
+			;;
+		esac
 		;;
 	a40|\
 	a60|\

tools/firmware-utils/src/tplink-safeloader.c
@@ -210,7 +210,17 @@ static struct device_info boards[] = {
 			"CPE510(TP-LINK|EU|N300-5):1.1\r\n"
 			"CPE520(TP-LINK|UN|N300-5):1.1\r\n"
 			"CPE520(TP-LINK|US|N300-5):1.1\r\n"
-			"CPE520(TP-LINK|EU|N300-5):1.1\r\n",
+			"CPE520(TP-LINK|EU|N300-5):1.1\r\n"
+			"CPE510(TP-LINK|EU|N300-5|00000000):2.0\r\n"
+			"CPE510(TP-LINK|EU|N300-5|45550000):2.0\r\n"
+			"CPE510(TP-LINK|EU|N300-5|55530000):2.0\r\n"
+			"CPE510(TP-LINK|UN|N300-5|00000000):2.0\r\n"
+			"CPE510(TP-LINK|UN|N300-5|45550000):2.0\r\n"
+			"CPE510(TP-LINK|UN|N300-5|55530000):2.0\r\n"
+			"CPE510(TP-LINK|US|N300-5|55530000):2.0\r\n"
+			"CPE510(TP-LINK|UN|N300-5):2.0\r\n"
+			"CPE510(TP-LINK|EU|N300-5):2.0\r\n"
+			"CPE510(TP-LINK|US|N300-5):2.0\r\n",
 		.support_trail = '\xff',
 		.soft_ver = NULL,
 

commit ade75d2b2de86f6421a2a0e74bad8f69d8fa456e

ar71xx: improve support for TP-Link CPE510 v2
This fixes commit 8c51c7fd48d6 ("ar71xx: add support for TP-LINK CPE510
V2.0") where the support for this device wasn't optimal.

Device support for the CPE510v2 so far has been a hack to enable
flashing with CPE510v1 images. Those even have different hardware (e.g.
additional ethernet port).

With this patch, we provide proper support for this device in ar71xx.

Installation:
- Flash factory image through stock firmware WEB UI or through TFTP
- To get to TFTP recovery just hold reset button while powering on
  for around 4-5 seconds and release.
- Rename factory image to recovery.bin
- Stock TFTP server IP: 192.168.0.100
- Stock device TFTP address: 192.168.0.254

Fixes: 8c51c7fd48d6 ("ar71xx: add support for TP-LINK CPE510 V2.0")
Signed-off-by: Andrew Cameron <apcameron@softhome.net>
[Rebased onto revert commit, changed comments in mach-cpe510.c,
changed commit title and description, fixed eth0 MAC address,
removed eth1 initialization]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
[squashed revert, added fixes tag]
Signed-off-by: Petr Štetiar <ynezz@true.cz>

target/linux/ar71xx/base-files/etc/board.d/01_leds
@@ -255,6 +255,7 @@ cpe210|\
 cpe210-v2|\
 cpe210-v3|\
 cpe510|\
+cpe510-v2|\
 wbs210|\
 wbs510)
 	ucidef_set_rssimon "wlan0" "200000" "1"
@@ -265,7 +266,8 @@ wbs510)
 
 	case "$board" in
 	cpe210-v2|\
-	cpe210-v3)
+	cpe210-v3|\
+	cpe510-v2)
 		ucidef_set_led_netdev "lan" "LAN" "tp-link:green:lan0" "eth0"
 		;;
 	*)

target/linux/ar71xx/base-files/etc/board.d/02_network
@@ -76,6 +76,7 @@ ar71xx_setup_interfaces()
 	cf-e380ac-v2|\
 	cpe210-v2|\
 	cpe210-v3|\
+	cpe510-v2|\
 	dr342|\
 	eap120|\
 	eap300v2|\

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -760,14 +760,11 @@ ar71xx_board_detect() {
 		;;
 	*"CPE510/520")
 		name="cpe510"
+		tplink_pharos_board_detect "$(tplink_pharos_get_model_string | tr -d '\r')"
+		;;
+	*"CPE510 v2")
+		name="cpe510-v2"
 		tplink_pharos_board_detect "$(tplink_pharos_v2_get_model_string)"
-		case $AR71XX_MODEL in
-		'TP-Link CPE510 v2.0')
-			;;
-		*)
-			tplink_pharos_board_detect "$(tplink_pharos_get_model_string | tr -d '\r')"
-			;;
-		esac
 		;;
 	*"CPE830")
 		name="cpe830"

target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -586,6 +586,7 @@ platform_check_image() {
 		return $?
 		;;
 	cpe210|\
+	cpe510|\
 	eap120|\
 	wbs210|\
 	wbs510)
@@ -597,19 +598,9 @@ platform_check_image() {
 		tplink_pharos_check_image "$1" "01000000" "$(tplink_pharos_v2_get_model_string)" '\0\xff\r' && return 0
 		return 1
 		;;
-	cpe510)
-		local modelstr="$(tplink_pharos_v2_get_model_string)"
-		tplink_pharos_board_detect $modelstr
-		case $AR71XX_MODEL in
-		'TP-Link CPE510 v2.0')
-			tplink_pharos_check_image "$1" "7f454c46" "$modelstr" '\0\xff\r' && return 0
-			return 1
-			;;
-		*)
-			tplink_pharos_check_image "$1" "7f454c46" "$(tplink_pharos_get_model_string)" '' && return 0
-			return 1
-			;;
-		esac
+	cpe510-v2)
+		tplink_pharos_check_image "$1" "7f454c46" "$(tplink_pharos_v2_get_model_string)" '\0\xff\r' && return 0
+		return 1
 		;;
 	a40|\
 	a60|\

target/linux/ar71xx/files/arch/mips/ath79/mach-cpe510.c
@@ -230,6 +230,35 @@ static void __init cpe210_v2_setup(void)
 	ath79_register_wmac(ee, mac);
 }
 
+static void __init cpe510_v2_setup(void)
+{
+	u8 *mac = (u8 *) KSEG1ADDR(0x1f830008);
+	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
+
+	/* disable PHY_SWAP and PHY_ADDR_SWAP bits */
+	ath79_setup_ar933x_phy4_switch(false, false);
+
+	ath79_register_leds_gpio(-1, ARRAY_SIZE(cpe210_v2_leds_gpio),
+					cpe210_v2_leds_gpio);
+	ath79_register_gpio_keys_polled(-1, CPE510_KEYS_POLL_INTERVAL,
+					ARRAY_SIZE(cpe510_gpio_keys),
+					cpe510_gpio_keys);
+
+	ath79_register_m25p80(NULL);
+	ath79_register_mdio(1, 0x0);
+
+	/* LAN port */
+	ath79_switch_data.phy4_mii_en = 1;
+	ath79_switch_data.phy_poll_mask = BIT(4);
+	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
+	ath79_eth0_data.phy_mask = BIT(4);
+	ath79_eth0_data.mii_bus_dev = &ath79_mdio1_device.dev;
+	ath79_init_mac(ath79_eth0_data.mac_addr, mac, 0);
+	ath79_register_eth(0);
+
+	ath79_register_wmac(ee, mac);
+}
+
 MIPS_MACHINE(ATH79_MACH_CPE210, "CPE210", "TP-LINK CPE210/220",
 	     cpe210_setup);
 
@@ -241,6 +270,9 @@ MIPS_MACHINE(ATH79_MACH_CPE210_V3, "CPE210V3", "TP-LINK CPE210 v3",
 
 MIPS_MACHINE(ATH79_MACH_CPE510, "CPE510", "TP-LINK CPE510/520",
 	     cpe510_setup);
+
+MIPS_MACHINE(ATH79_MACH_CPE510_V2, "CPE510V2", "TP-LINK CPE510 v2",
+	     cpe510_v2_setup);
 
 MIPS_MACHINE(ATH79_MACH_WBS210, "WBS210", "TP-LINK WBS210",
 	     wbs_setup);

target/linux/ar71xx/files/arch/mips/ath79/machtypes.h
@@ -79,6 +79,7 @@ enum ath79_mach_type {
 	ATH79_MACH_CPE210_V3,			/* TP-LINK CPE210 v3 */
 	ATH79_MACH_CPE505N,			/* P&W CPE505N */
 	ATH79_MACH_CPE510,			/* TP-LINK CPE510 */
+	ATH79_MACH_CPE510_V2,			/* TP-LINK CPE510 v2 */
 	ATH79_MACH_CPE830,			/* YunCore CPE830 */
 	ATH79_MACH_CPE870,			/* YunCore CPE870 */
 	ATH79_MACH_CR3000,			/* PowerCloud Systems CR3000 */

target/linux/ar71xx/image/generic-tp-link.mk
@@ -177,6 +177,14 @@ define Device/cpe510-520-v1
 endef
 TARGET_DEVICES += cpe510-520-v1
 
+define Device/cpe510-v2
+  $(Device/cpe510-520-v1)
+  DEVICE_TITLE := TP-LINK CPE510 v2
+  BOARDNAME := CPE510V2
+  TPLINK_BOARD_ID := CPE510V2
+endef
+TARGET_DEVICES += cpe510-v2
+
 define Device/cpe210-220-v1
   $(Device/cpe510-520-v1)
   DEVICE_TITLE := TP-LINK CPE210/220 v1

tools/firmware-utils/src/tplink-safeloader.c
@@ -271,7 +271,7 @@ static struct device_info boards[] = {
 		.last_sysupgrade_partition = "support-list",
 	},
 
-	/** Firmware layout for the CPE510/520 */
+	/** Firmware layout for the CPE510/520 V1 */
 	{
 		.id	= "CPE510",
 		.vendor	= "CPE510(TP-LINK|UN|N300-5):1.0\r\n",
@@ -284,17 +284,7 @@ static struct device_info boards[] = {
 			"CPE510(TP-LINK|EU|N300-5):1.1\r\n"
 			"CPE520(TP-LINK|UN|N300-5):1.1\r\n"
 			"CPE520(TP-LINK|US|N300-5):1.1\r\n"
-			"CPE520(TP-LINK|EU|N300-5):1.1\r\n"
-			"CPE510(TP-LINK|EU|N300-5|00000000):2.0\r\n"
-			"CPE510(TP-LINK|EU|N300-5|45550000):2.0\r\n"
-			"CPE510(TP-LINK|EU|N300-5|55530000):2.0\r\n"
-			"CPE510(TP-LINK|UN|N300-5|00000000):2.0\r\n"
-			"CPE510(TP-LINK|UN|N300-5|45550000):2.0\r\n"
-			"CPE510(TP-LINK|UN|N300-5|55530000):2.0\r\n"
-			"CPE510(TP-LINK|US|N300-5|55530000):2.0\r\n"
-			"CPE510(TP-LINK|UN|N300-5):2.0\r\n"
-			"CPE510(TP-LINK|EU|N300-5):2.0\r\n"
-			"CPE510(TP-LINK|US|N300-5):2.0\r\n",
+			"CPE520(TP-LINK|EU|N300-5):1.1\r\n",
 		.support_trail = '\xff',
 		.soft_ver = NULL,
 