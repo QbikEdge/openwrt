QbikEdge/openwrt

commit 713f3d2149dd340dd50695a74f95f1e46fc529fa

ath79: split base-files into subtargets
While most of the target's contents are split into subtargets, the
base-files are maintained for the target as a whole.

However, OpenWrt already implements a mechanism that will use (and
even prefer) files in the subtargets' directories. This can be
exploited to make several scripts subtarget-specific and thus save
some space (especially helpful for the tiny devices).

The only script remaining in parent base-files is
/etc/hotplug.d/ieee80211/00-wifi-migration, everything else is
moved/split.

Note that this will increase overall code lines, but reduce code
per subtarget.

base-files ipk size reduction:
master (generic)   49135 B
split (generic)    48533 B (- 0.6 kiB)
split (tiny)       43337 B (- 5.7 kiB)
split (nand)       44423 B (- 4.6 kiB)

Tested on TL-WR1043ND v4 (generic) and TL-WR841N v12 (tiny).

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/generic/base-files/etc/board.d/02_network
@@ -28,7 +28,6 @@ ath79_setup_interfaces()
 	pisen,ts-d084|\
 	pisen,wmb001n|\
 	pisen,wmm003n|\
-	pqi,air-pen|\
 	tplink,cpe210-v2|\
 	tplink,cpe210-v3|\
 	tplink,cpe510-v2|\
@@ -38,11 +37,6 @@ ath79_setup_interfaces()
 	tplink,re355-v1|\
 	tplink,re450-v1|\
 	tplink,re450-v2|\
-	tplink,tl-mr10u|\
-	tplink,tl-mr3020-v1|\
-	tplink,tl-mr3040-v2|\
-	tplink,tl-wa901nd-v2|\
-	tplink,tl-wr703n|\
 	ubnt,bullet-m|\
 	ubnt,bullet-m-xw|\
 	ubnt,lap-120|\
@@ -76,26 +70,11 @@ ath79_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0@eth1" "1:lan" "2:lan" "3:lan" "4:lan" "5:wan" "6@eth0"
 		;;
-	buffalo,whr-g301n)
-		ucidef_set_interface_wan "eth0"
-		ucidef_add_switch "switch0" \
-			"0@eth1" "1:lan:4" "2:lan:3" "3:lan:2" "4:lan:1"
-		;;
 	buffalo,wzr-hp-ag300h|\
 	tplink,archer-c25-v1|\
-	tplink,tl-mr3220-v1|\
-	tplink,tl-mr3420-v1|\
 	tplink,tl-wdr3500-v1|\
-	tplink,tl-wr841-v7|\
-	tplink,tl-wr841-v9|\
-	tplink,tl-wr841-v10|\
-	tplink,tl-wr841-v11|\
-	tplink,tl-wr841-v12|\
 	tplink,tl-wr842n-v1|\
 	tplink,tl-wr842n-v3|\
-	tplink,tl-wr940n-v3|\
-	tplink,tl-wr940n-v4|\
-	tplink,tl-wr941nd-v6|\
 	ubnt,airrouter)
 		ucidef_set_interface_wan "eth1"
 		ucidef_add_switch "switch0" \
@@ -205,20 +184,6 @@ ath79_setup_interfaces()
 		ucidef_add_switch_port_attr "switch0" 2 led 9
 		ucidef_add_switch_port_attr "switch0" 5 led 2
 		;;
-	netgear,wnr1000-v2|\
-	netgear,wnr2000-v3|\
-	netgear,wnr612-v2|\
-	on,n150r|\
-	tplink,tl-wr740n-v1|\
-	tplink,tl-wr740n-v3|\
-	tplink,tl-wr741-v1|\
-	tplink,tl-wr743nd-v1|\
-	tplink,tl-wr841-v5|\
-	tplink,tl-wr941-v4)
-		ucidef_set_interface_wan "eth0"
-		ucidef_add_switch "switch0" \
-		"0@eth1" "1:lan" "2:lan" "3:lan" "4:lan"
-		;;
 	phicomm,k2t)
 		ucidef_add_switch "switch0" \
 			"0@eth0" "3:lan:1" "5:lan:2" "4:wan"
@@ -237,8 +202,7 @@ ath79_setup_interfaces()
 	tplink,archer-c7-v4|\
 	tplink,archer-c7-v5|\
 	tplink,tl-wdr3600-v1|\
-	tplink,tl-wdr4300-v1|\
-	tplink,tl-wr941n-v7-cn)
+	tplink,tl-wdr4300-v1)
 		ucidef_add_switch "switch0" \
 			"0@eth0" "2:lan:1" "3:lan:2" "4:lan:3" "5:lan:4" "1:wan"
 		;;
@@ -259,10 +223,6 @@ ath79_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0@eth0" "2:lan:3" "3:lan:2" "4:lan:1" "1:wan"
 		;;
-	tplink,tl-mr3420-v2|\
-	tplink,tl-wr740n-v4|\
-	tplink,tl-wr741nd-v4|\
-	tplink,tl-wr841-v8|\
 	tplink,tl-wr842n-v2)
 		ucidef_set_interface_wan "eth1"
 		ucidef_add_switch "switch0" \
@@ -280,9 +240,6 @@ ath79_setup_interfaces()
 	tplink,tl-wr2543-v1)
 		ucidef_add_switch "switch0" \
 			"1:lan" "2:lan" "3:lan" "4:lan" "0:wan" "9@eth0"
-		;;
-	tplink,tl-wr941-v2)
-		ucidef_set_interfaces_lan_wan "lan1 lan2 lan3 lan4" "wan"
 		;;
 	ubnt,acb-isp)
 		ucidef_set_interface_wan "eth1"
@@ -427,11 +384,6 @@ ath79_setup_macs()
 	tplink,tl-wr1043nd-v4|\
 	tplink,tl-wr1043n-v5)
 		base_mac=$(mtd_get_mac_binary info 0x8)
-		wan_mac=$(macaddr_add "$base_mac" 1)
-		;;
-	tplink,tl-wr941-v2|\
-	tplink,tl-wr941n-v7-cn)
-		base_mac=$(mtd_get_mac_binary u-boot 0x1fc00)
 		wan_mac=$(macaddr_add "$base_mac" 1)
 		;;
 	trendnet,tew-823dru)

target/linux/ath79/generic/base-files/etc/hotplug.d/firmware/10-ath9k-eeprom
@@ -61,10 +61,7 @@ case "$FIRMWARE" in
 	avm,fritz300e)
 		caldata_extract_reverse "urloader" 0x1541 0x440
 		;;
-	buffalo,whr-g301n|\
-	buffalo,wzr-hp-g302h-a1a0|\
-	tplink,tl-wr841-v5|\
-	tplink,tl-wr941-v4)
+	buffalo,wzr-hp-g302h-a1a0)
 		caldata_extract "art" 0x1000 0xeb8
 		;;
 	buffalo,wzr-hp-g450h)
@@ -83,28 +80,14 @@ case "$FIRMWARE" in
 	winchannel,wb2000)
 		caldata_extract "art" 0x5000 0x440
 		;;
-	netgear,wnr1000-v2|\
-	netgear,wnr2000-v3|\
-	netgear,wnr612-v2|\
-	on,n150r|\
 	pcs,cap324|\
-	tplink,tl-mr3220-v1|\
-	tplink,tl-mr3420-v1|\
 	tplink,tl-wr2543-v1|\
-	tplink,tl-wr740n-v1|\
-	tplink,tl-wr740n-v3|\
-	tplink,tl-wr741-v1|\
-	tplink,tl-wr743nd-v1|\
-	tplink,tl-wr841-v7|\
 	tplink,tl-wr842n-v1|\
 	ubnt,airrouter|\
 	ubnt,bullet-m|\
 	ubnt,nano-m|\
 	ubnt,rocket-m)
 		caldata_extract "art" 0x1000 0x1000
-		;;
-	pqi,air-pen)
-		caldata_extract "art" 0x1000 0x7d2
 		;;
 	ubnt,unifi)
 		caldata_extract "art" 0x1000 0x800

target/linux/ath79/nand/base-files/etc/board.d/02_network
@@ -1,0 +1,35 @@
+#!/bin/sh
+
+. /lib/functions/system.sh
+. /lib/functions/uci-defaults.sh
+
+ath79_setup_interfaces()
+{
+	local board="$1"
+
+	case "$board" in
+	*)
+		ucidef_set_interfaces_lan_wan "eth0" "eth1"
+		;;
+	esac
+}
+
+ath79_setup_macs()
+{
+	local board="$1"
+
+	case "$board" in
+	esac
+
+	[ -n "$lan_mac" ] && ucidef_set_interface_macaddr "lan" $lan_mac
+	[ -n "$wan_mac" ] && ucidef_set_interface_macaddr "wan" $wan_mac
+	[ -n "$label_mac" ] && ucidef_set_label_macaddr $label_mac
+}
+
+board_config_update
+board=$(board_name)
+ath79_setup_interfaces $board
+ath79_setup_macs $board
+board_config_flush
+
+exit 0

target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
@@ -1,0 +1,20 @@
+#
+# Copyright (C) 2011 OpenWrt.org
+#
+
+PART_NAME=firmware
+REQUIRE_IMAGE_METADATA=1
+
+platform_check_image() {
+	return 0
+}
+
+platform_do_upgrade() {
+	local board=$(board_name)
+
+	case "$board" in
+	*)
+		default_do_upgrade "$1"
+		;;
+	esac
+}

target/linux/ath79/tiny/base-files/etc/board.d/02_network
@@ -1,0 +1,96 @@
+#!/bin/sh
+
+. /lib/functions/system.sh
+. /lib/functions/uci-defaults.sh
+
+ath79_setup_interfaces()
+{
+	local board="$1"
+
+	case "$board" in
+	pqi,air-pen|\
+	tplink,tl-mr10u|\
+	tplink,tl-mr3020-v1|\
+	tplink,tl-mr3040-v2|\
+	tplink,tl-wa901nd-v2|\
+	tplink,tl-wr703n)
+		ucidef_set_interface_lan "eth0"
+		;;
+	buffalo,whr-g301n)
+		ucidef_set_interface_wan "eth0"
+		ucidef_add_switch "switch0" \
+			"0@eth1" "1:lan:4" "2:lan:3" "3:lan:2" "4:lan:1"
+		;;
+	tplink,tl-mr3220-v1|\
+	tplink,tl-mr3420-v1|\
+	tplink,tl-wr841-v7|\
+	tplink,tl-wr841-v9|\
+	tplink,tl-wr841-v10|\
+	tplink,tl-wr841-v11|\
+	tplink,tl-wr841-v12|\
+	tplink,tl-wr940n-v3|\
+	tplink,tl-wr940n-v4|\
+	tplink,tl-wr941nd-v6)
+		ucidef_set_interface_wan "eth1"
+		ucidef_add_switch "switch0" \
+			"0@eth0" "1:lan:4" "2:lan:3" "3:lan:2" "4:lan:1"
+		;;
+	netgear,wnr1000-v2|\
+	netgear,wnr2000-v3|\
+	netgear,wnr612-v2|\
+	on,n150r|\
+	tplink,tl-wr740n-v1|\
+	tplink,tl-wr740n-v3|\
+	tplink,tl-wr741-v1|\
+	tplink,tl-wr743nd-v1|\
+	tplink,tl-wr841-v5|\
+	tplink,tl-wr941-v4)
+		ucidef_set_interface_wan "eth0"
+		ucidef_add_switch "switch0" \
+		"0@eth1" "1:lan" "2:lan" "3:lan" "4:lan"
+		;;
+	tplink,tl-wr941n-v7-cn)
+		ucidef_add_switch "switch0" \
+			"0@eth0" "2:lan:1" "3:lan:2" "4:lan:3" "5:lan:4" "1:wan"
+		;;
+	tplink,tl-mr3420-v2|\
+	tplink,tl-wr740n-v4|\
+	tplink,tl-wr741nd-v4|\
+	tplink,tl-wr841-v8)
+		ucidef_set_interface_wan "eth1"
+		ucidef_add_switch "switch0" \
+			"0@eth0" "1:lan:4" "2:lan:1" "3:lan:2" "4:lan:3"
+		;;
+	tplink,tl-wr941-v2)
+		ucidef_set_interfaces_lan_wan "lan1 lan2 lan3 lan4" "wan"
+		;;
+	*)
+		ucidef_set_interfaces_lan_wan "eth0" "eth1"
+		;;
+	esac
+}
+
+ath79_setup_macs()
+{
+	local board="$1"
+
+	case "$board" in
+	tplink,tl-wr941-v2|\
+	tplink,tl-wr941n-v7-cn)
+		base_mac=$(mtd_get_mac_binary u-boot 0x1fc00)
+		wan_mac=$(macaddr_add "$base_mac" 1)
+		;;
+	esac
+
+	[ -n "$lan_mac" ] && ucidef_set_interface_macaddr "lan" $lan_mac
+	[ -n "$wan_mac" ] && ucidef_set_interface_macaddr "wan" $wan_mac
+	[ -n "$label_mac" ] && ucidef_set_label_macaddr $label_mac
+}
+
+board_config_update
+board=$(board_name)
+ath79_setup_interfaces $board
+ath79_setup_macs $board
+board_config_flush
+
+exit 0

target/linux/ath79/tiny/base-files/etc/hotplug.d/firmware/10-ath9k-eeprom
@@ -1,0 +1,38 @@
+#!/bin/sh
+
+[ -e /lib/firmware/$FIRMWARE ] && exit 0
+
+. /lib/functions/caldata.sh
+
+board=$(board_name)
+
+case "$FIRMWARE" in
+"ath9k-eeprom-pci-0000:00:00.0.bin")
+	case $board in
+	buffalo,whr-g301n|\
+	tplink,tl-wr841-v5|\
+	tplink,tl-wr941-v4)
+		caldata_extract "art" 0x1000 0xeb8
+		;;
+	netgear,wnr1000-v2|\
+	netgear,wnr2000-v3|\
+	netgear,wnr612-v2|\
+	on,n150r|\
+	tplink,tl-mr3220-v1|\
+	tplink,tl-mr3420-v1|\
+	tplink,tl-wr740n-v1|\
+	tplink,tl-wr740n-v3|\
+	tplink,tl-wr741-v1|\
+	tplink,tl-wr743nd-v1|\
+	tplink,tl-wr841-v7)
+		caldata_extract "art" 0x1000 0x1000
+		;;
+	pqi,air-pen)
+		caldata_extract "art" 0x1000 0x7d2
+		;;
+	*)
+		caldata_die "board $board is not supported yet"
+		;;
+	esac
+	;;
+esac

commit eaafa1e0ce65bd7b27df76218142d31230560709

ath79: move base-files for ZyXEL NBG6716
Due to rebase of the base-files split patch after adding support
for the ZyXEL NBG6716, its base-files were added to the generic
subtarget, although the device belongs to the nand subtarget.

This moves the definitions to their proper locations.

Fixes: 713f3d2149dd ("ath79: split base-files into subtargets")

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>
[improve commit message, rebase]
Signed-off-by: David Bauer <mail@david-bauer.net>

target/linux/ath79/generic/base-files/etc/board.d/02_network
@@ -261,10 +261,6 @@ ath79_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0@eth0" "3:lan:1" "4:lan:2"
 		;;
-	zyxel,nbg6716)
-		ucidef_add_switch "switch0" \
-			"0@eth0" "1:lan" "2:lan" "3:lan" "4:lan" "5:wan" "6@eth1"
-		;;
 	*)
 		ucidef_set_interfaces_lan_wan "eth0" "eth1"
 		;;
@@ -398,10 +394,6 @@ ath79_setup_macs()
 		;;
 	wd,mynet-wifi-rangeextender)
 		lan_mac=$(nvram get et0macaddr)
-		;;
-	zyxel,nbg6716)
-		lan_mac=$(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +2)
-		wan_mac=$(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +3)
 		;;
 	esac
 

target/linux/ath79/generic/base-files/etc/hotplug.d/firmware/10-ath9k-eeprom
@@ -47,10 +47,6 @@ case "$FIRMWARE" in
 		caldata_extract "radiocfg" 0x1000 0x440
 		ath9k_patch_mac $(mtd_get_mac_ascii devdata "wlan24mac")
 		;;
-	zyxel,nbg6716)
-		caldata_extract "art" 0x1000 0x440
-		ath9k_patch_mac $(mtd_get_mac_ascii u-boot-env ethaddr)
-		;;
 	*)
 		caldata_die "board $board is not supported yet"
 		;;

target/linux/ath79/generic/base-files/etc/hotplug.d/firmware/11-ath10k-caldata
@@ -113,10 +113,6 @@ case "$FIRMWARE" in
 	ubnt,unifiac-pro)
 		caldata_extract "EEPROM" 0x5000 0x844
 		;;
-	zyxel,nbg6716)
-		caldata_extract "art" 0x5000 0x844
-		ath10kcal_patch_mac $(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +1)
-		;;
 	esac
 	;;
 "ath10k/pre-cal-pci-0000:00:00.0.bin")

target/linux/ath79/generic/base-files/lib/upgrade/platform.sh
@@ -51,9 +51,6 @@ platform_do_upgrade() {
 	ubnt,routerstation-pro)
 		echo "Sysupgrade disabled due bug FS#2428"
 		;;
-	zyxel,nbg6716)
-		nand_do_upgrade "$1"
-		;;
 	*)
 		default_do_upgrade "$1"
 		;;

target/linux/ath79/nand/base-files/etc/board.d/02_network
@@ -11,6 +11,10 @@ ath79_setup_interfaces()
 	aerohive,hiveap-121)
 		ucidef_set_interface_lan "eth0"
 		;;
+	zyxel,nbg6716)
+		ucidef_add_switch "switch0" \
+			"0@eth0" "1:lan" "2:lan" "3:lan" "4:lan" "5:wan" "6@eth1"
+		;;
 	*)
 		ucidef_set_interfaces_lan_wan "eth0" "eth1"
 		;;
@@ -22,6 +26,10 @@ ath79_setup_macs()
 	local board="$1"
 
 	case "$board" in
+	zyxel,nbg6716)
+		lan_mac=$(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +2)
+		wan_mac=$(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +3)
+		;;
 	esac
 
 	[ -n "$lan_mac" ] && ucidef_set_interface_macaddr "lan" $lan_mac

target/linux/ath79/nand/base-files/etc/hotplug.d/firmware/10-ath9k-eeprom
@@ -1,0 +1,21 @@
+#!/bin/sh
+
+[ -e /lib/firmware/$FIRMWARE ] && exit 0
+
+. /lib/functions/caldata.sh
+
+board=$(board_name)
+
+case "$FIRMWARE" in
+"ath9k-eeprom-ahb-18100000.wmac.bin")
+	case $board in
+	zyxel,nbg6716)
+		caldata_extract "art" 0x1000 0x440
+		ath9k_patch_mac $(mtd_get_mac_ascii u-boot-env ethaddr)
+		;;
+	*)
+		caldata_die "board $board is not supported yet"
+		;;
+	esac
+	;;
+esac

target/linux/ath79/nand/base-files/etc/hotplug.d/firmware/11-ath10k-caldata
@@ -1,0 +1,22 @@
+#!/bin/sh
+
+[ -e /lib/firmware/$FIRMWARE ] && exit 0
+
+. /lib/functions/caldata.sh
+. /lib/functions/k2t.sh
+
+board=$(board_name)
+
+case "$FIRMWARE" in
+"ath10k/cal-pci-0000:00:00.0.bin")
+	case $board in
+	zyxel,nbg6716)
+		caldata_extract "art" 0x5000 0x844
+		ath10kcal_patch_mac $(macaddr_add $(mtd_get_mac_ascii u-boot-env ethaddr) +1)
+		;;
+	esac
+	;;
+*)
+	exit 1
+	;;
+esac

target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
@@ -13,7 +13,8 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	aerohive,hiveap-121)
+	aerohive,hiveap-121|\
+	zyxel,nbg6716)
 		nand_do_upgrade "$1"
 		;;
 	*)