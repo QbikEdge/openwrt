QbikEdge/openwrt

commit f29ef49be32b2666d76a034b1371c8c2fdf42b9f

x86: add nforce eth to default packages
forcedeth is necessary to use the integrated
ethernet controller of Nvidia nForce chipset.

There are PC motherboards with this chipset
from 2001 that run 32bit Athlon XP CPUs and
more modern ones up to 2009 that can run Intel
and AMD 64bit processors, so add this to
all non-geode x86 targets.

Signed-off-by: Alberto Bursi <bobafetthotmail@gmail.com>

target/linux/x86/64/target.mk
@@ -1,6 +1,7 @@
 ARCH:=x86_64
 BOARDNAME:=x86_64
-DEFAULT_PACKAGES += kmod-button-hotplug kmod-e1000e kmod-e1000 kmod-r8169 kmod-igb kmod-bnx2
+DEFAULT_PACKAGES += kmod-button-hotplug kmod-e1000e kmod-e1000 kmod-r8169 \
+	kmod-igb kmod-bnx2 forcedeth
 
 define Target/Description
         Build images for 64 bit systems including virtualized guests.

target/linux/x86/image/generic.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_TITLE := Generic x86
   DEVICE_PACKAGES += kmod-3c59x kmod-8139too kmod-e100 kmod-e1000 kmod-natsemi \
 	kmod-ne2k-pci kmod-pcnet32 kmod-r8169 kmod-sis900 kmod-tg3 \
-	kmod-via-rhine kmod-via-velocity
+	kmod-via-rhine kmod-via-velocity forcedeth
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic

target/linux/x86/image/legacy.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_TITLE := Generic x86/legacy
   DEVICE_PACKAGES += kmod-3c59x kmod-8139too kmod-e100 kmod-e1000 \
 	kmod-natsemi kmod-ne2k-pci kmod-pcnet32 kmod-r8169 kmod-sis900 \
-	kmod-tg3 kmod-via-rhine kmod-via-velocity
+	kmod-tg3 kmod-via-rhine kmod-via-velocity forcedeth
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic

commit e9fd67de8ab0fc4602a448d750a25c32df496729

x86: fix kmod-forcedeth package selection
There's no such package as forcedeth, threfore the driver is never
selected. Fix it by properly specifying package name.

Fixes: f29ef49 ("x86: add nforce eth to default packages")
Signed-off-by: Tomasz Maciej Nowak <tomek_n@o2.pl>

target/linux/x86/64/target.mk
@@ -1,7 +1,7 @@
 ARCH:=x86_64
 BOARDNAME:=x86_64
 DEFAULT_PACKAGES += kmod-button-hotplug kmod-e1000e kmod-e1000 kmod-r8169 \
-	kmod-igb kmod-bnx2 forcedeth
+	kmod-igb kmod-bnx2 kmod-forcedeth
 
 define Target/Description
         Build images for 64 bit systems including virtualized guests.

target/linux/x86/image/generic.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_TITLE := Generic x86
   DEVICE_PACKAGES += kmod-3c59x kmod-8139too kmod-e100 kmod-e1000 kmod-natsemi \
 	kmod-ne2k-pci kmod-pcnet32 kmod-r8169 kmod-sis900 kmod-tg3 \
-	kmod-via-rhine kmod-via-velocity forcedeth
+	kmod-via-rhine kmod-via-velocity kmod-forcedeth
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic

target/linux/x86/image/legacy.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_TITLE := Generic x86/legacy
   DEVICE_PACKAGES += kmod-3c59x kmod-8139too kmod-e100 kmod-e1000 \
 	kmod-natsemi kmod-ne2k-pci kmod-pcnet32 kmod-r8169 kmod-sis900 \
-	kmod-tg3 kmod-via-rhine kmod-via-velocity forcedeth
+	kmod-tg3 kmod-via-rhine kmod-via-velocity kmod-forcedeth
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic