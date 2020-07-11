QbikEdge/openwrt

commit 8fdd48a5dd44baad67b3ebcdb94a7e30d6319c1d

firmware-utils: mkfwimage: fix more errors reported by gcc-6/7/9
src/mkfwimage.c:276:8: error: format '%lld' expects argument of type 'long long int', but argument 4 has type '__off_t' {aka 'const long int'} [-Werror=format=]
src/fw.h:71:36: error: format '%llu' expects argument of type 'long long unsigned int', but argument 6 has type '__off_t' {aka 'long int'} [-Werror=format=]
    inlined from 'main' at src/mkfwimage.c:543:12:
/string_fortified.h:106:10: error: '__builtin_strncpy' output truncated before terminating nul copying 4 bytes from a string of the same length [-Werror=stringop-truncation]
    inlined from 'write_part' at src/mkfwimage.c:235:2,
string_fortified.h:106:10: error: '__builtin_strncpy' specified bound 16 equals destination size [-Werror=stringop-truncation]
    inlined from 'main' at src/mkfwimage.c:477:5:
string_fortified.h:106:10: error: '__builtin_strncpy' specified bound 256 equals destination size [-Werror=stringop-truncation]
    inlined from 'main' at src/mkfwimage.c:496:5:
string_fortified.h:106:10: error: '__builtin_strncpy' specified bound 4096 equals destination size [-Werror=stringop-truncation]
    inlined from 'main' at src/mkfwimage.c:481:5:
string_fortified.h:106:10: error: '__builtin_strncpy' specified bound 4096 equals destination size [-Werror=stringop-truncation]
    inlined from 'main' at src/mkfwimage.c:485:5:
string_fortified.h:106:10: error: '__builtin_strncpy' specified bound 16 equals destination size [-Werror=stringop-truncation]

Runtested on ath79 and UBNT Bullet M XW.

Signed-off-by: Petr Štetiar <ynezz@true.cz>

tools/firmware-utils/src/fw.h
@@ -27,6 +27,7 @@
 #define MAGIC_ENDS	"ENDS"
 
 #define MAGIC_LENGTH	4
+#define PART_NAME_LENGTH 16
 
 typedef struct header {
 	char magic[MAGIC_LENGTH];
@@ -37,7 +38,7 @@ typedef struct header {
 
 typedef struct part {
 	char magic[MAGIC_LENGTH];
-	char name[16];
+	char name[PART_NAME_LENGTH];
 	char pad[12];
 	u_int32_t memaddr;
 	u_int32_t index;

tools/firmware-utils/src/mkfwimage.c
@@ -230,9 +230,10 @@ static int write_part(void* mem, part_data_t* d)
 	memcpy(mem + sizeof(part_t), addr, d->stats.st_size);
 	munmap(addr, d->stats.st_size);
 
-	memset(p->name, 0, sizeof(p->name));
-	strncpy(p->magic, MAGIC_PART, MAGIC_LENGTH);
-	strncpy(p->name, d->partition_name, sizeof(p->name));
+	memset(p->name, 0, PART_NAME_LENGTH);
+	memcpy(p->magic, MAGIC_PART, MAGIC_LENGTH);
+	memcpy(p->name, d->partition_name, PART_NAME_LENGTH);
+
 	p->index = htonl(d->partition_index);
 	p->data_size = htonl(d->stats.st_size);
 	p->part_size = htonl(d->partition_length);
@@ -273,7 +274,7 @@ static void print_image_info(const image_info_t* im)
 	for (i = 0; i < im->part_count; ++i)
 	{
 		const part_data_t* d = &im->parts[i];
-		INFO(" %10s: %8lld bytes (free: %8lld)\n",
+		INFO(" %10s: %8ld bytes (free: %8ld)\n",
 		     d->partition_name,
 		     d->stats.st_size,
 		     d->partition_length - d->stats.st_size);
@@ -372,7 +373,7 @@ static int validate_image_layout(image_info_t* im)
 			return -3;
 		}
 		if (d->stats.st_size > d->partition_length) {
-			ERROR("File '%s' too big (%d) - max size: 0x%08X (exceeds %llu bytes)\n",
+			ERROR("File '%s' too big (%d) - max size: 0x%08X (exceeds %lu bytes)\n",
 				       	d->filename, i, d->partition_length,
 					d->stats.st_size - d->partition_length);
 			return -4;
@@ -474,30 +475,30 @@ int main(int argc, char* argv[])
 		switch (o) {
 		case 'v':
 			if (optarg)
-				strncpy(im.version, optarg, sizeof(im.version));
+				strncpy(im.version, optarg, sizeof(im.version) - 1);
 			break;
 		case 'o':
 			if (optarg)
-				strncpy(im.outputfile, optarg, sizeof(im.outputfile));
+				strncpy(im.outputfile, optarg, sizeof(im.outputfile) - 1);
 			break;
 		case 'm':
 			if (optarg)
-				strncpy(im.magic, optarg, sizeof(im.magic));
+				strncpy(im.magic, optarg, sizeof(im.magic) - 1);
 			break;
 		case 'h':
 			usage(argv[0]);
 			return -1;
 		case 'k':
 			if (optarg)
-				strncpy(kernelfile, optarg, sizeof(kernelfile));
+				strncpy(kernelfile, optarg, sizeof(kernelfile) - 1);
 			break;
 		case 'r':
 			if (optarg)
-				strncpy(rootfsfile, optarg, sizeof(rootfsfile));
+				strncpy(rootfsfile, optarg, sizeof(rootfsfile) - 1);
 			break;
 		case 'B':
 			if (optarg)
-				strncpy(board_name, optarg, sizeof(board_name));
+				strncpy(board_name, optarg, sizeof(board_name) - 1);
 			break;
 		}
 	}

tools/firmware-utils/Makefile
@@ -38,7 +38,7 @@ define Host/Compile
 	$(call cc,mkzynfw)
 	$(call cc,lzma2eva,-lz)
 	$(call cc,mkcasfw)
-	$(call cc,mkfwimage,-lz -Wall -Werror -Wextra)
+	$(call cc,mkfwimage,-lz -Wall -Werror -Wextra -D_FILE_OFFSET_BITS=64)
 	$(call cc,mkfwimage2,-lz)
 	$(call cc,imagetag imagetag_cmdline cyg_crc32)
 	$(call cc,add_header)

commit 8ac8c9826e4a10b168256afc0be36c799d00e233

firmware-utils: mkfwimage: fix memcpy and strncpy usage
Firmware is binary blob, so there are barely any NULL terminated strings
expected, so we should probably convert all chars into u8 types, and
after that it's clear, that using strcpy doesn't make sense anymore.

This is rather theoretical stuff, but `uint8_t name[PART_NAME_LENGTH]`
means, that you can supply PART_NAME_LENGTH sized name, not
PART_NAME_LENGTH-1 name when NULL terminated.

Ref: https://github.com/openwrt/openwrt/pull/2274
Fixes: 8fdd48a5dd44 ("firmware-utils: mkfwimage: fix more errors reported by gcc-6/7/9")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

tools/firmware-utils/src/fw.h
@@ -19,6 +19,7 @@
 #ifndef FW_INCLUDED
 #define FW_INCLUDED
 
+#include <stdint.h>
 #include <sys/types.h>
 
 #define MAGIC_HEADER	"OPEN"
@@ -30,16 +31,16 @@
 #define PART_NAME_LENGTH 16
 
 typedef struct header {
-	char magic[MAGIC_LENGTH];
-	char version[256];
+	uint8_t magic[MAGIC_LENGTH];
+	uint8_t version[256];
 	u_int32_t crc;
 	u_int32_t pad;
 } __attribute__ ((packed)) header_t;
 
 typedef struct part {
-	char magic[MAGIC_LENGTH];
-	char name[PART_NAME_LENGTH];
-	char pad[12];
+	uint8_t magic[MAGIC_LENGTH];
+	uint8_t name[PART_NAME_LENGTH];
+	uint8_t pad[12];
 	u_int32_t memaddr;
 	u_int32_t index;
 	u_int32_t baseaddr;
@@ -54,13 +55,13 @@ typedef struct part_crc {
 } __attribute__ ((packed)) part_crc_t;
 
 typedef struct signature {
-	char magic[MAGIC_LENGTH];
+	uint8_t magic[MAGIC_LENGTH];
 	u_int32_t crc;
 	u_int32_t pad;
 } __attribute__ ((packed)) signature_t;
 
 typedef struct signature_rsa {
-	char magic[MAGIC_LENGTH];
+	uint8_t magic[MAGIC_LENGTH];
 //	u_int32_t crc;
 	unsigned char rsa_signature[256];
 	u_int32_t pad;

tools/firmware-utils/src/mkfwimage.c
@@ -32,6 +32,7 @@
 #include <limits.h>
 #include <stdbool.h>
 #include "fw.h"
+#include "utils.h"
 
 typedef struct fw_layout_data {
 	u_int32_t	kern_start;
@@ -204,13 +205,12 @@ static void write_header(void* mem, const char *magic, const char* version)
 	header_t* header = mem;
 	memset(header, 0, sizeof(header_t));
 
-	memcpy(header->magic, magic, MAGIC_LENGTH);
-	strncpy(header->version, version, sizeof(header->version));
-	header->crc = htonl(crc32(0L, (unsigned char *)header,
-				sizeof(header_t) - 2 * sizeof(u_int32_t)));
+	FW_MEMCPY_STR(header->magic, magic);
+	FW_MEMCPY_STR(header->version, version);
+	header->crc = htonl(crc32(0L, (uint8_t*) header,
+			    sizeof(header_t) - 2 * sizeof(u_int32_t)));
 	header->pad = 0L;
 }
-
 
 static void write_signature(void* mem, u_int32_t sig_offset)
 {
@@ -218,7 +218,7 @@ static void write_signature(void* mem, u_int32_t sig_offset)
 	signature_t* sign = (signature_t*)(mem + sig_offset);
 	memset(sign, 0, sizeof(signature_t));
 
-	memcpy(sign->magic, MAGIC_END, MAGIC_LENGTH);
+	FW_MEMCPY_STR(sign->magic, MAGIC_END);
 	sign->crc = htonl(crc32(0L,(unsigned char *)mem, sig_offset));
 	sign->pad = 0L;
 }
@@ -229,7 +229,7 @@ static void write_signature_rsa(void* mem, u_int32_t sig_offset)
 	signature_rsa_t* sign = (signature_rsa_t*)(mem + sig_offset);
 	memset(sign, 0, sizeof(signature_rsa_t));
 
-	memcpy(sign->magic, MAGIC_ENDS, MAGIC_LENGTH);
+	FW_MEMCPY_STR(sign->magic, MAGIC_ENDS);
 //	sign->crc = htonl(crc32(0L,(unsigned char *)mem, sig_offset));
 	sign->pad = 0L;
 }
@@ -259,8 +259,8 @@ static int write_part(void* mem, part_data_t* d)
 	munmap(addr, d->stats.st_size);
 
 	memset(p->name, 0, PART_NAME_LENGTH);
-	memcpy(p->magic, MAGIC_PART, MAGIC_LENGTH);
-	memcpy(p->name, d->partition_name, PART_NAME_LENGTH);
+	FW_MEMCPY_STR(p->magic, MAGIC_PART);
+	FW_MEMCPY_STR(p->name, d->partition_name);
 
 	p->index = htonl(d->partition_index);
 	p->data_size = htonl(d->stats.st_size);

tools/firmware-utils/src/utils.h
@@ -1,0 +1,11 @@
+#include <stdint.h>
+#include <string.h>
+
+#pragma once
+
+#define FW_MEMCPY_STR(dst, src)					\
+       do {							\
+               size_t slen = strlen(src);			\
+               size_t dlen = sizeof(dst);			\
+               memcpy(dst, src, slen > dlen ? dlen : slen);	\
+       } while (0);