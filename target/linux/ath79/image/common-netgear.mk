QbikEdge/openwrt

commit 74e3f2485a99aad5268b03c086779284170ea7b9

ath79: netgear: trim down uImage customisations
Replace NETGEAR_KERNEL_MAGIC by UIMAGE_MAGIC to better match the
variable's purpose. This allows to drop the custom
Build/netgear-uImage.

Signed-off-by: Sander Vanheule <sander@svanheule.net>
[keep UIMAGE_MAGIC definitions even for default value]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/common-netgear.mk
@@ -1,9 +1,9 @@
-DEVICE_VARS += NETGEAR_KERNEL_MAGIC NETGEAR_BOARD_ID NETGEAR_HW_ID
+DEVICE_VARS += NETGEAR_BOARD_ID NETGEAR_HW_ID
 
 define Build/netgear-rootfs
 	mkimage \
 		-A mips -O linux -T filesystem -C none \
-		-M $(NETGEAR_KERNEL_MAGIC) \
+		$(if $(UIMAGE_MAGIC),-M $(UIMAGE_MAGIC)) \
 		-n '$(VERSION_DIST) filesystem' \
 		-d $(IMAGE_ROOTFS) $@.fs
 	cat $@.fs >> $@
@@ -22,21 +22,17 @@ define Build/netgear-squashfs
 	dd if=/dev/zero bs=1k count=1 >> $@.squashfs
 	mkimage \
 		-A mips -O linux -T filesystem -C none \
-		-M $(NETGEAR_KERNEL_MAGIC) \
+		$(if $(UIMAGE_MAGIC),-M $(UIMAGE_MAGIC)) \
 		-a 0xbf070000 -e 0xbf070000 \
 		-n 'MIPS $(VERSION_DIST) Linux-$(LINUX_VERSION)' \
 		-d $@.squashfs $@
 	rm -rf $@.squashfs $@.fs
 endef
 
-define Build/netgear-uImage
-	$(call Build/uImage,$(1) -M $(NETGEAR_KERNEL_MAGIC))
-endef
-
 define Device/netgear_generic
   DEVICE_VENDOR := NETGEAR
-  KERNEL := kernel-bin | append-dtb | lzma -d20 | netgear-uImage lzma
-  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | netgear-uImage lzma
+  KERNEL := kernel-bin | append-dtb | lzma -d20 | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | uImage lzma
   IMAGES += factory.img
   IMAGE/default := append-kernel | pad-to $$$$(BLOCKSIZE) | netgear-squashfs | \
 	append-rootfs | pad-rootfs

target/linux/ath79/image/nand.mk
@@ -1,5 +1,3 @@
-include ./common-netgear.mk	# for netgear-uImage
-
 DEVICE_VARS += RAS_ROOTFS_SIZE RAS_BOARD RAS_VERSION
 
 # attention: only zlib compression is allowed for the boot fs
@@ -163,10 +161,10 @@ define Device/netgear_ath79_nand
   PAGESIZE := 2048
   IMAGE_SIZE := 25600k
   KERNEL := kernel-bin | append-dtb | lzma -d20 | \
-	pad-offset $$(KERNEL_SIZE) 129 | netgear-uImage lzma | \
+	pad-offset $$(KERNEL_SIZE) 129 | uImage lzma | \
 	append-string -e '\xff' | \
-	append-uImage-fakehdr filesystem $$(NETGEAR_KERNEL_MAGIC)
-  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | netgear-uImage lzma
+	append-uImage-fakehdr filesystem $$(UIMAGE_MAGIC)
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma -d20 | uImage lzma
   IMAGES := sysupgrade.bin factory.img
   IMAGE/factory.img := append-kernel | append-ubi | netgear-dni | \
 	check-size
@@ -179,7 +177,7 @@ define Device/netgear_wndr3700-v4
   SOC := ar9344
   DEVICE_MODEL := WNDR3700
   DEVICE_VARIANT := v4
-  NETGEAR_KERNEL_MAGIC := 0x33373033
+  UIMAGE_MAGIC := 0x33373033
   NETGEAR_BOARD_ID := WNDR3700v4
   NETGEAR_HW_ID := 29763948+128+128
   $(Device/netgear_ath79_nand)
@@ -189,7 +187,7 @@ TARGET_DEVICES += netgear_wndr3700-v4
 define Device/netgear_wndr4300
   SOC := ar9344
   DEVICE_MODEL := WNDR4300
-  NETGEAR_KERNEL_MAGIC := 0x33373033
+  UIMAGE_MAGIC := 0x33373033
   NETGEAR_BOARD_ID := WNDR4300
   NETGEAR_HW_ID := 29763948+0+128+128+2x2+3x3
   $(Device/netgear_ath79_nand)
@@ -199,7 +197,7 @@ TARGET_DEVICES += netgear_wndr4300
 define Device/netgear_wndr4300sw
   SOC := ar9344
   DEVICE_MODEL := WNDR4300SW
-  NETGEAR_KERNEL_MAGIC := 0x33373033
+  UIMAGE_MAGIC := 0x33373033
   NETGEAR_BOARD_ID := WNDR4300SW
   NETGEAR_HW_ID := 29763948+0+128+128+2x2+3x3
   $(Device/netgear_ath79_nand)
@@ -209,7 +207,7 @@ TARGET_DEVICES += netgear_wndr4300sw
 define Device/netgear_wndr4300tn
   SOC := ar9344
   DEVICE_MODEL := WNDR4300TN
-  NETGEAR_KERNEL_MAGIC := 0x33373033
+  UIMAGE_MAGIC := 0x33373033
   NETGEAR_BOARD_ID := WNDR4300TN
   NETGEAR_HW_ID := 29763948+0+128+128+2x2+3x3
   $(Device/netgear_ath79_nand)
@@ -220,7 +218,7 @@ define Device/netgear_wndr4300-v2
   SOC := qca9563
   DEVICE_MODEL := WNDR4300
   DEVICE_VARIANT := v2
-  NETGEAR_KERNEL_MAGIC := 0x27051956
+  UIMAGE_MAGIC := 0x27051956
   NETGEAR_BOARD_ID := WNDR4500series
   NETGEAR_HW_ID := 29764821+2+128+128+3x3+3x3+5508012175
   $(Device/netgear_ath79_nand)
@@ -231,7 +229,7 @@ define Device/netgear_wndr4500-v3
   SOC := qca9563
   DEVICE_MODEL := WNDR4500
   DEVICE_VARIANT := v3
-  NETGEAR_KERNEL_MAGIC := 0x27051956
+  UIMAGE_MAGIC := 0x27051956
   NETGEAR_BOARD_ID := WNDR4500series
   NETGEAR_HW_ID := 29764821+2+128+128+3x3+3x3+5508012173
   $(Device/netgear_ath79_nand)

target/linux/ath79/image/generic.mk
@@ -1225,7 +1225,7 @@ TARGET_DEVICES += nec_wg800hp
 define Device/netgear_ex6400_ex7300
   $(Device/netgear_generic)
   SOC := qca9558
-  NETGEAR_KERNEL_MAGIC := 0x27051956
+  UIMAGE_MAGIC := 0x27051956
   NETGEAR_BOARD_ID := EX7300series
   NETGEAR_HW_ID := 29765104+16+0+128
   IMAGE_SIZE := 15552k
@@ -1261,7 +1261,7 @@ define Device/netgear_wndr3700
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDR3700
   DEVICE_VARIANT := v1
-  NETGEAR_KERNEL_MAGIC := 0x33373030
+  UIMAGE_MAGIC := 0x33373030
   NETGEAR_BOARD_ID := WNDR3700
   IMAGE_SIZE := 7680k
   IMAGES += factory-NA.img
@@ -1275,7 +1275,7 @@ define Device/netgear_wndr3700-v2
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDR3700
   DEVICE_VARIANT := v2
-  NETGEAR_KERNEL_MAGIC := 0x33373031
+  UIMAGE_MAGIC := 0x33373031
   NETGEAR_BOARD_ID := WNDR3700v2
   NETGEAR_HW_ID := 29763654+16+64
   IMAGE_SIZE := 15872k
@@ -1286,7 +1286,7 @@ TARGET_DEVICES += netgear_wndr3700-v2
 define Device/netgear_wndr3800
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDR3800
-  NETGEAR_KERNEL_MAGIC := 0x33373031
+  UIMAGE_MAGIC := 0x33373031
   NETGEAR_BOARD_ID := WNDR3800
   NETGEAR_HW_ID := 29763654+16+128
   IMAGE_SIZE := 15872k
@@ -1297,7 +1297,7 @@ TARGET_DEVICES += netgear_wndr3800
 define Device/netgear_wndr3800ch
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDR3800CH
-  NETGEAR_KERNEL_MAGIC := 0x33373031
+  UIMAGE_MAGIC := 0x33373031
   NETGEAR_BOARD_ID := WNDR3800CH
   NETGEAR_HW_ID := 29763654+16+128
   IMAGE_SIZE := 15872k
@@ -1309,7 +1309,7 @@ define Device/netgear_wndrmac-v1
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDRMAC
   DEVICE_VARIANT := v1
-  NETGEAR_KERNEL_MAGIC := 0x33373031
+  UIMAGE_MAGIC := 0x33373031
   NETGEAR_BOARD_ID := WNDRMAC
   NETGEAR_HW_ID := 29763654+16+64
   IMAGE_SIZE := 15872k
@@ -1321,7 +1321,7 @@ define Device/netgear_wndrmac-v2
   $(Device/netgear_wndr3x00)
   DEVICE_MODEL := WNDRMAC
   DEVICE_VARIANT := v2
-  NETGEAR_KERNEL_MAGIC := 0x33373031
+  UIMAGE_MAGIC := 0x33373031
   NETGEAR_BOARD_ID := WNDRMACv2
   NETGEAR_HW_ID := 29763654+16+128
   IMAGE_SIZE := 15872k
@@ -1334,7 +1334,7 @@ define Device/netgear_wnr2200_common
   SOC := ar7241
   DEVICE_MODEL := WNR2200
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport
-  NETGEAR_KERNEL_MAGIC := 0x32323030
+  UIMAGE_MAGIC := 0x32323030
   NETGEAR_BOARD_ID := wnr2200
 endef
 

target/linux/ath79/image/tiny-netgear.mk
@@ -6,7 +6,7 @@ define Device/netgear_wnr612-v2
   DEVICE_MODEL := WNR612
   DEVICE_VARIANT := v2
   DEVICE_DTS := ar7240_netgear_wnr612-v2
-  NETGEAR_KERNEL_MAGIC := 0x32303631
+  UIMAGE_MAGIC := 0x32303631
   NETGEAR_BOARD_ID := REALWNR612V2
   IMAGE_SIZE := 3712k
   SUPPORTED_DEVICES += wnr612-v2
@@ -19,7 +19,7 @@ define Device/on_n150r
   SOC := ar7240
   DEVICE_VENDOR := On Networks
   DEVICE_MODEL := N150R
-  NETGEAR_KERNEL_MAGIC := 0x32303631
+  UIMAGE_MAGIC := 0x32303631
   NETGEAR_BOARD_ID := N150R
   IMAGE_SIZE := 3712k
   SUPPORTED_DEVICES += n150r
@@ -32,7 +32,7 @@ define Device/netgear_wnr1000-v2
   SOC := ar7240
   DEVICE_MODEL := WNR1000
   DEVICE_VARIANT := v2
-  NETGEAR_KERNEL_MAGIC := 0x31303031
+  UIMAGE_MAGIC := 0x31303031
   NETGEAR_BOARD_ID := WNR1000V2
   NETGEAR_HW_ID := 29763331+4+32
   IMAGE_SIZE := 3712k
@@ -46,7 +46,7 @@ define Device/netgear_wnr2000-v3
   SOC := ar7241
   DEVICE_MODEL := WNR2000
   DEVICE_VARIANT := v3
-  NETGEAR_KERNEL_MAGIC := 0x32303033
+  UIMAGE_MAGIC := 0x32303033
   NETGEAR_BOARD_ID := WNR2000V3
   NETGEAR_HW_ID := 29763551+04+32
   IMAGE_SIZE := 3712k

commit 8786c93e6a5f1f07fec7ffcd6b36262ef364d255

ath79: fix factory image generation for Netgear and Zyxel boards
The factory images need to embed specific IDs to pass verification with
the OEM firmware (including TFTP recovery), so they need to be
per-device variables.

Fixes: 74e3f2485a99 ("ath79: netgear: trim down uImage customisations")
Fixes: fb2d9b54f77a ("ath79: add support for ZyXEL NBG6616")
Reported-by: Marcin Juszkiewicz <marcin-openwrt@juszkiewicz.com.pl>
Signed-off-by: Paul Fertser <fercerpav@gmail.com>
[minor commit message adjustments, sort DEVICE_VARS]
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/Makefile
@@ -4,6 +4,8 @@ include $(INCLUDE_DIR)/image.mk
 KERNEL_LOADADDR = 0x80060000
 
 DEVICE_VARS += LOADER_FLASH_OFFS LOADER_TYPE
+DEVICE_VARS += NETGEAR_BOARD_ID NETGEAR_HW_ID
+DEVICE_VARS += RAS_BOARD RAS_ROOTFS_SIZE RAS_VERSION
 
 define Build/combined-image
 	sh $(TOPDIR)/scripts/combined-image.sh \

target/linux/ath79/image/common-netgear.mk
@@ -1,5 +1,3 @@
-DEVICE_VARS += NETGEAR_BOARD_ID NETGEAR_HW_ID
-
 define Build/netgear-rootfs
 	mkimage \
 		-A mips -O linux -T filesystem -C none \

target/linux/ath79/image/nand.mk
@@ -1,5 +1,3 @@
-DEVICE_VARS += RAS_ROOTFS_SIZE RAS_BOARD RAS_VERSION
-
 # attention: only zlib compression is allowed for the boot fs
 define Build/zyxel-buildkerneljffs
 	rm -rf  $(KDIR_TMP)/zyxelnbg6716