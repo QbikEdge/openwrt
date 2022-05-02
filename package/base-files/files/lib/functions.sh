QbikEdge/openwrt

commit 8581ea995ce052215f35ba9fb0d1dd8bb9eeb921

base-files: address sed in-place without SELinux awareness
sed(1) in busybox does not support this functionality:
https://git.savannah.gnu.org/cgit/sed.git/tree/sed/execute.c#n598

This causes /etc/group to become mislabeled when a package requests
that a uid/gid be added on OpenWrt with SELinux

Signed-off-by: Daniel Golle <daniel@makrotopia.org>
[move restorecon inside lock]
Signed-off-by: Dominick Grift <dominick.grift@defensec.nl>

package/base-files/files/lib/functions.sh
@@ -386,6 +386,7 @@ group_add_user() {
 	echo "$grp" | grep -q ":$" && delim=""
 	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
 	sed -i "s/$grp/$grp$delim$2/g" ${IPKG_INSTROOT}/etc/group
+	selinuxenabled 2>/dev/null && restorecon /etc/group
 	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
 }
 

commit f7d24e365f50a2c6fd01ae2724651e0638a0aae4

base-files: add missing $IPKG_INSTROOT to restorecon call
Update to overlooked v2 version of Dominick Grift's patch.

Fixes: 8581ea995c ("base-files: address sed in-place without SELinux awareness")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/base-files/files/lib/functions.sh
@@ -386,7 +386,9 @@ group_add_user() {
 	echo "$grp" | grep -q ":$" && delim=""
 	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
 	sed -i "s/$grp/$grp$delim$2/g" ${IPKG_INSTROOT}/etc/group
-	selinuxenabled 2>/dev/null && restorecon /etc/group
+	if [ -x /usr/sbin/selinuxenabled ] && selinuxenabled; then
+		selinuxenabled 2>/dev/null && restorecon ${IPKG_INSTROOT}/etc/group
+	fi
 	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
 }
 