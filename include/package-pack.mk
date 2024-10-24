QbikEdge/openwrt

commit aa24769e0c70dba4535b9c99937b85262d04e20f

include/package-pack: set missing Description on .apk creation
Set missing description info on .apk creation. This was probably a TODO
that wasn't notice when the final implementation was pushed.

Fixes: e5f9382a8b44 ("build: add APK package build capabilities")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description:" \
+	  --info "description: $$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \

commit 281ea63c767b987c5d7bb77b84d886a2f7ade8d2

include/package-pack: remove whitespace for Description info for APK
Remove whitespace for Description info for .apk creation

Fixes: aa24769e0c70 ("include/package-pack: set missing Description on .apk creation")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

include/package-pack.mk
@@ -339,7 +339,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  --info "description: $$(strip $$(Package/$(1)/description))" \
+	  --info "description:$$(strip $$(Package/$(1)/description))" \
 	  --info "arch:$(PKGARCH)" \
 	  --info "license:$(LICENSE)" \
 	  --info "origin:$(SOURCE)" \