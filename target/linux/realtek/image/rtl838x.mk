QbikEdge/openwrt

commit 77af0c1234638000525b1abc053b51c58ec984c6

realtek: rename hpe,1920-8g-poe to match hardware
There are two hardware models of the HPE 1920-8g-poe switch. The version
currently in the repository is the model with a PoE budget of 180W. In
preparation of the addition of the 65W model, the existing model is
renamed to clarify the hardware version it targets.

As suggested by Pawel, the 'SUPPORTED_DEVICES' includes the old target
name to enable an upgrade path of builds with the old name.

Suggested-by: Pawel Dembicki <paweldembicki@gmail.com>
Signed-off-by: Kevin Jilissen <info@kevinjilissen.nl>

target/linux/realtek/image/rtl838x.mk
@@ -99,13 +99,14 @@ define Device/hpe_1920-8g
 endef
 TARGET_DEVICES += hpe_1920-8g
 
-define Device/hpe_1920-8g-poe
+define Device/hpe_1920-8g-poe-180w
   $(Device/hpe_1920)
   SOC := rtl8380
-  DEVICE_MODEL := 1920-8G-PoE+ (JG922A)
+  DEVICE_MODEL := 1920-8G-PoE+ 180W (JG922A)
   H3C_DEVICE_ID := 0x00010025
+  SUPPORTED_DEVICES += hpe_1920-8g-poe
 endef
-TARGET_DEVICES += hpe_1920-8g-poe
+TARGET_DEVICES += hpe_1920-8g-poe-180w
 
 define Device/hpe_1920-16g
   $(Device/hpe_1920)

target/linux/realtek/base-files/etc/board.d/02_network
@@ -31,7 +31,7 @@ lan_mac_end=""
 label_mac=""
 case $board in
 hpe,1920-8g|\
-hpe,1920-8g-poe|\
+hpe,1920-8g-poe-180w|\
 hpe,1920-16g|\
 hpe,1920-24g)
 	label_mac=$(mtd_get_mac_binary factory 0x68)
@@ -67,7 +67,7 @@ done
 [ -n "$label_mac" ] && ucidef_set_label_macaddr $label_mac
 
 case $board in
-hpe,1920-8g-poe)
+hpe,1920-8g-poe-180w)
 	ucidef_set_poe 180 "$lan_list_rev" "lan9 lan10"
 	;;
 netgear,gs110tpp-v1)

target/linux/realtek/base-files/etc/board.d/03_gpio_switches
@@ -6,7 +6,7 @@ board_config_update
 board=$(board_name)
 
 case "$board" in
-hpe,1920-8g-poe)
+hpe,1920-8g-poe-180w)
 	ucidef_add_gpio_switch "fan_ctrl" "Fan control" "456" "0"
 	;;
 esac

target/linux/realtek/dts-5.15/rtl8380_hpe_1920-8g-poe-180w.dts
@@ -3,8 +3,8 @@
 #include "rtl8380_hpe_1920-8g.dtsi"
 
 / {
-	compatible = "hpe,1920-8g-poe", "realtek,rtl838x-soc";
-	model = "HPE 1920-8G-PoE+ (JG922A)";
+	compatible = "hpe,1920-8g-poe-180w", "realtek,rtl838x-soc";
+	model = "HPE 1920-8G-PoE+ 180W (JG922A)";
 };
 
 &uart1 {

commit 6fdeb0bd7e3f0c89bcbaded01ef8b183af8fe2cc

realtek: Fix old compatible for HPE 1920-8G PoE
Supported devices are listed in the metadata as the first part of the
DTS compatible. This normally follows the format "vendor,device".

When updating the device name of the 180W 1920-8G PoE an underscore was
used, instead of a comma, to join the vendor and device name. This will
lead to warnings for users wanting to sysupgrade a device with an older
compatible, as the device's info does not match the one the metadata.

Fixes: 77af0c123463 ("realtek: rename hpe,1920-8g-poe to match hardware")
Signed-off-by: Sander Vanheule <sander@svanheule.net>

target/linux/realtek/image/rtl838x.mk
@@ -125,7 +125,7 @@ define Device/hpe_1920-8g-poe-180w
   DEVICE_MODEL := 1920-8G-PoE+ 180W (JG922A)
   DEVICE_PACKAGES += realtek-poe kmod-hwmon-gpiofan
   H3C_DEVICE_ID := 0x00010025
-  SUPPORTED_DEVICES += hpe_1920-8g-poe
+  SUPPORTED_DEVICES += hpe,1920-8g-poe
 endef
 TARGET_DEVICES += hpe_1920-8g-poe-180w
 